64e1f5eab6536b2c1b1555d5c20b822f5db9d839
dragun8 <rjslsla12@naver.com>
2025-09-17 23:19:49 +0900

    jang

--- Algorithm/배용건/장훈이의선반높이/janghoon.py
+++ Algorithm/배용건/장훈이의선반높이/janghoon.py
@@ -1,27 +1,40 @@
+import sys
+sys.stdin = open('input.txt','r')
+
+
+def dfs(idx, h_sum):
+    global answer
+    if h_sum >= B:
+        answer = min(h_sum, answer - B)
+        return
+    
+    if answer == 0:
+        return
+    
+    if idx == N:
+        return
+    dfs(idx+1,h_sum + heights[idx])
+    dfs(idx+1,h_sum)
+
+
+
 T = int(input())
 
 for tc in range(1, T + 1):
     N, B = map(int, input().split())
     heights = list(map(int, input().split()))
 
-    answer = [float('inf')] # 초과 높이중 최소높이
 
-    def dfs(idx, total):
+    heights.sort(reverse=True)
+    answer = float('inf') # 초과 높이중 최소높이
+
+    
         
-
-        if total >= B:
-            answer = min(answer, total - B)
-            return
-
-        if idx == N:
-            return
-        
-        dfs(idx + 1, total + heights[idx])
-        # idx 번째 점원을 사용
-
-        dfs(idx + 1, total)
-        # idx 번째 점원을 사용하지 않는다
     dfs(0, 0) # 점원 0번부터, 누적합 0으로 DFS 시작
+    
+    if answer == float('inf'):
+        answer = 0
+    
     print(f"#{tc} {answer}")
 
 
